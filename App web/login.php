d6ac22a63270eeb51c891b09c6b610b4c509515b
Faical Bahsis <65980103+faical23@users.noreply.github.com>
2021-03-03 10:52:36 +0100

    finish header and footer

--- App web/login.php
+++ App web/login.php
@@ -8,29 +8,6 @@
     <title>Login</title>
 </head>
 <body class="body_login">
-    <?php include  "assets/php/header.php"?>
-    <section class="login_hero">
-        <div class="container">
-            <div class="form_and_centent">
-                <div class="content">
-                    <h1>Feel free to connect
-                        with you YOUCODE 
-                        account
-                        </h1>
-                </div>
-                <div class="zon_form">
-                    <form action=""  method="POST">
-                        <h3>Sign up</h3>
-                        <input type="email" placeholder="Email" name="email">
-                        <input type="password" placeholder="Password" name="password">
-                        <button class="" name="btn_login">Login</button>
-                        <P>Forgot password?</P>
-                    </form>
-                </div>
 
-            </div>
-        </div>
-    </section>
-    <script src="assets/js/main.js"></script>
 </body>
 </html>
