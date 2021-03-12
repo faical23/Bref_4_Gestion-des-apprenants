507e03777e3ac7429f954449a228dcbf37dad8ec
Faical Bahsis <65980103+faical23@users.noreply.github.com>
2021-03-12 12:02:01 +0100

    add element

--- App web/assets/sql/index.sql
+++ App web/assets/sql/index.sql
@@ -1,7 +1,6 @@
 Create Database getion_apprenents;    /*  create databe*/
-drop Database if exists getion_apprenents; /* delete  database */
 
-create table 2student (  /* create student tabel */
+create table student (  /* create student tabel */
 
     id INT PRIMARY KEY NOT NULL,
     nom VARCHAR(100) NOT NULL,
@@ -11,28 +10,22 @@ create table 2student (  /* create student tabel */
     Html int default "0",
     Css int default "0",
     Javascript int default "0",
-    Php int default "0"
+    Php int default "0",
+    class varchar(255)
 )
 
-
-CREATE TABLE formateur AS   /* create table from abther exist table */
+CREATE TABLE formateur AS   /* create table from another exist table */
 SELECT id ,nom, prenom , email
 FROM student;
-
-ALTER TABLE formateur_youcode   /* change name of table */
-RENAME TO formateur;  
-
-DROP tabel formateur; /* delete a table */
 
 ALTER TABLE formateur  /* add attrubite to  exists table */
 ADD password_formateur VARCHAR(255) NOT NULL ;
 
-ALTER TABLE student /* delete attrubite for exist table */
-DROP id;
-
-
 /* add a row from a table */
 INSERT INTO student (nom,prenom,email,password_student,Html,Css,Javascript,Php) VALUES("faical","bahsis","faissalabr@gmail.com","faical99",16,15,14,13)
+
+/* add a row from a table */
+INSERT INTO formateur (id,nom,prenom,email,password_formateur) VALUES("chaimae","nmeyess","chaima@gmail.com","chaimae123")
 
 
 /* update a attrubite from a eow table */
@@ -46,3 +39,11 @@ WHERE nom = "faical"; /* condition should exists */
 
 DELETE FROM student
 WHERE nom = "faical" AND prenom = "bahsis";
+
+
+ALTER TABLE formateur_youcode   /* change name of table */
+RENAME TO formateur;  
+
+
+ALTER TABLE student /* delete attrubite for exist table */
+DROP class;
