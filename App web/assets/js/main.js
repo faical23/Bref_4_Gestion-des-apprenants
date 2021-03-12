ba735914baa272704b57a38e42d55d2827e27555
Faical Bahsis <65980103+faical23@users.noreply.github.com>
2021-03-12 09:58:13 +0100

    add js file

--- App web/assets/js/main.js
+++ App web/assets/js/main.js
@@ -1,3 +1,125 @@
 
 
 
+
+	/*  validation regix */
+
+	var inputName = document.getElementById("name")
+	var inputPrenom = document.getElementById("prenom")
+	var inputEmail = document.getElementById("email")
+	var inputPassword = document.getElementById("password")
+	
+	function validation_Name(par,regix , nmber_char){
+		var input_name = document.querySelector(`#${par}`);
+		var input_name_value = input_name.value; 
+		console.log("true");
+		console.log(input_name_value);
+		input_name.style="border:1px solid red ";
+
+			if(input_name_value .match(regix) && input_name_value .length >= nmber_char) { 
+				document.getElementById("valide").disabled = false; 
+				input_name.style="border:1px solid green ";
+
+			}
+			else{
+				document.getElementById("valide").disabled = true; 
+			}
+
+	}
+		inputName.addEventListener('keyup', () => {
+			validation_Name('name' , /^[a-zA-Z ]+$/ ,4)
+		})
+		inputPrenom.addEventListener('keyup', () => {
+			validation_Name('prenom' , /^[a-zA-Z ]+$/ ,4)
+		})
+		inputEmail.addEventListener('keyup', () => {
+			validation_Name('email' , /^[^ ]+@[^ ]+\.[a-z]{2,3}$/ , 5)
+		})
+		inputPassword.addEventListener('keyup', () => {
+			validation_Name('password' , /^[A-Za-z0-9]\w{5,}$/ , 5)
+		})
+
+
+	
+/* show popup data */
+
+	var popup_see_note = document.querySelector(".popup_see_note");
+
+	var pop_up_show = true 
+
+var see_data = (id_user , name  , prenom ,email , password , html , css , js , php) =>{
+
+	if(pop_up_show == true)
+	{
+
+		popup_see_note.style="display:grid !important";
+
+		popup_see_note.innerHTML = `<h1>${name} ${prenom}</h1>
+								<p>Class : <span>Adalovelace</span></p>
+								<p>Email : <span>${email}</span></p>
+								<p>Password : <span>${password}</span></p>
+								<h2>Notes</h2>
+								<p>HTML : <span>${html}</span></p>
+								<p>CSS : <span>${css}</span></p>
+								<p>JAVASCRIPT : <span>${js}</span></p>
+								<p>PHP : <span>${php}</span></p>
+
+								<div class="btn_popup_okey">
+									<button class="button_okey" onclick="pop_up_display()">Okey</button>
+						</div>`;
+
+
+	}
+
+}
+var button_okey = document.querySelector(".button_okey");
+
+pop_up_display = () =>{
+	popup_see_note.style="display:none !important";
+}
+
+
+/* delete data */
+
+function delete_popup(id_user){
+
+
+		swal({
+		title: "Are you sure?",
+		text: "Once deleted, you will not be able to recover this imaginary file!",
+		icon: "warning",
+		buttons: true,
+		dangerMode: true,
+		})
+		.then((willDelete) => {
+		if (willDelete) {
+			window.location.href = `assets/php/delete.php?id=${id_user}`;
+		} else {
+			swal("Your imaginary file is safe!");
+		}
+		});
+
+}
+
+/* get data to update  */
+
+function imported(user_id,user_name , user_prenom , user_email , user_pass , user_html ,user_css,user_js , user_php){
+
+
+	document.getElementById("id_user").value=user_id;
+	document.getElementById("name").value=user_name;
+	document.getElementById("prenom").value=user_prenom;
+	document.getElementById("email").value=user_email;
+	document.getElementById("password").value=user_pass;
+	document.getElementById("html").value=user_html;
+	document.getElementById("css").value=user_css;
+	document.getElementById("js").value=user_js;
+	document.getElementById("php").value=user_php;
+	document.getElementById("update_btn").style="display:block";
+	document.getElementById("valide").style="display:none";		
+
+	console.log(id);
+}
+
+
+
